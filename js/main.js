cf631d106fa4b7d2b3dfa874326cd85849de4f63
R56123 <c4010606@hallam.shu.ac.uk>
2024-10-30 22:51:14 +0000

    Colour back through for loop.

---
+++ js/main.js
@@ -0,0 +1,40 @@
+console.info("Hello JS");
+
+document.getElementById("myHeading").innerHTML = "Rameen Burdabar";
+
+document.querySelector("nav>ul>li").setAttribute("class", "currentPage");
+
+document.querySelector("nav ul li a").setAttribute("href", "https://www.google.co.uk");
+
+let colourButtons = document.querySelectorAll(".colPicker");
+console.dir(colourButtons);
+
+
+for(let i=0; i< colourButtons.length; i++)
+    
+{
+colourButtons[i].style.backgroundColor = "grey";
+        
+colourButtons[i].addEventListener("click", function() {
+    if (colourButtons[i].classList.contains("red")) 
+        {
+        document.body.setAttribute("class" , "redBack");
+        }
+    else if (colourButtons[i].classList.contains("green")) 
+        {
+        document.body.setAttribute("class", "greenBack")
+        }
+    else if (colourButtons[1].classList.contains("blue")) 
+        {
+        document.body.setAttribute("class", "blueBack")
+        }
+    else if (colourButtons[i].classList.contains("reset")) 
+        {
+        document.body.removeAttribute("class");
+        }
+    });
+}
+    
+document.getElementById("myTestBtn").addEventListener("click", function() {
+console.info("Hi, I was clicked");
+});
